c63b16f48ec3e3a18a268b5ae878588e6881b522
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-07-16 15:31:51 +0530

    Added custom substring function

---
+++ custom_strstr.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int find_substring(const char *haystack, const char *needle) {
+	
+	int i = 0;
+	
+	while (haystack[i] != '\0'){
+		
+		if (haystack[i] == needle[0]){
+			
+			int j = 0;
+			
+			while (needle[j] != '\0' && haystack[i + j] != '\0' && haystack[i + j] == needle[j]){
+				j++;
+			}
+			
+			if (needle[j] == '\0') {
+				return i;
+			}
+		}
+		
+		i++;
+	}
+       
+	return -1;
+
+}
+
+int main() {
+	
+	char haystack[100];
+	char needle[100];
+	
+	fgets(haystack, sizeof(haystack), stdin);
+       	fgets(needle, sizeof(needle), stdin);
+	
+	int i = 0;
+	
+	while (haystack[i] != '\0') {
+		if (haystack[i] == '\n') {
+			haystack[i] = '\0';
+			break;
+		}
+		i++;
+	}
+	
+	i = 0;
+	
+	while (needle[i] != '\0') {
+		if (needle[i] == '\n') {
+			needle[i] = '\0';
+			break;
+		}
+		
+		i++;
+	}
+	
+	int index = find_substring(haystack, needle);
+	
+	printf("%d\n", index);
+       
+	return 0;
+}
