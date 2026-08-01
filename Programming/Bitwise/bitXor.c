521ccbb16deb5543d501e32300c3c2c4bc3b9f21
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-08-01 11:29:23 +0530

    Add bit xor

---
+++ Programming/Bitwise/bitXor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int bitXor(int x, int y){
+
+    return ~(~(~x & y) & ~(x & ~y));
+}
+
+int main(){
+
+    int x,y;
+
+    scanf("%d %d",&x,&y);
+
+    int ans = bitXor(x,y);
+
+    printf("%d\n",ans);
+}
+
