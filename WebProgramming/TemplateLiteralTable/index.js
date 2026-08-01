b80b14706d299b2e7db153daf5c86d6172031614
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-08-01 09:49:33 +0530

    Add generate table using template literals

---
+++ WebProgramming/TemplateLiteralTable/index.js
@@ -0,0 +1,23 @@
+const users = [
+    {name:"Alice", age:53},
+    {name:"Tim", age:74}
+]
+
+
+function generateTable(){
+
+    return `
+    <table>
+        ${users.map((user) => {
+            return `
+            <tr>
+               <td> ${user.name} </td>
+               <td> ${user.age} </td>
+            </tr>
+            `;
+        }).join("")}
+    </table>
+    `;
+}
+
+console.log(generateTable());
