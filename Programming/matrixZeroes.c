17f550c7ca6472ca974b6f12ce599f2585219b0d
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-08-06 14:47:34 +0530

    Add Set Matrix Zero

---
+++ Programming/matrixZeroes.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void setZeroes(int matrix[][31], int n , int m){
+
+    int col0 = 1;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(int j=0;j<m;j++){
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(col0 == 0){
+        for(int i=0;i<n;i++){
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+
+int main() {
+
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    int matrix[31][31];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    setZeroes(matrix, n, m);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
