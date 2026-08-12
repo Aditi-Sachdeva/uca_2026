f66abd9057eeea02f5d57419e16e7f9c649047b9
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-08-12 15:28:26 +0530

    Add Finding numbers in array problem

---
+++ Week6/Programming/FindingNumbersInArray.java
@@ -0,0 +1,32 @@
+package Week6.Programming;
+
+public class FindingNumbersInArray {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 1, 2, 4, 1, 2, 3, 4, 3, 3 };
+        System.out.println(findNumber(arr));
+
+    }
+
+    public static int findNumber(int[] arr) {
+
+        int ans = 0;
+
+        for (int bit = 0; bit < 32; bit++) {
+
+            int count = 0;
+
+            for (int num : arr) {
+                if ((num & (1 << bit)) != 0) {
+                    count++;
+                }
+            }
+
+            if (count % 3 == 1) {
+                ans = ans | (1 << bit);
+            }
+        }
+
+        return ans;
+    }
+}
